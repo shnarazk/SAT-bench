89c7d70de6b817938e7f3c6ac0402e2c84aa988d
shnarazk <narazakilaboratory@gmail.com>
2017-10-16 09:07:07 +0900

    mkScatter.R: accept a graph name as 3rd arg

--- bin/mkScatter.R
+++ bin/mkScatter.R
@@ -1,7 +1,5 @@
 #!/usr/bin/env Rscript
-# USAGE:
-# R_LIBS_USER=~/Sources/R
-# cat mkSATgraph.R | R --vanilla
+# USAGE: R_LIBS_USER=~/Sources/R scatter.R data1.csv data2.csv [graphName]")
 library("ggplot2")
 
 graph <- function (d, lx, ly) {
@@ -12,13 +10,13 @@ graph <- function (d, lx, ly) {
 #      g <- g + theme(legend.position = c(0.88,0.1), legend.justification = c(1,0))
 #      g <- g + scale_x_continuous(limits=c(0,1000),breaks=seq(0,1100,100))
 #      g <- g + scale_y_continuous(limits=c(0,810) ,breaks=seq(0,810,100))
-#      g <- g + xlab("#solved") + ylab("execution time [sec]") # + ggtitle("Cactus Plot on SAT-Race 2017 main track small subset")
+#      g <- g + xlab("#solved") + ylab("execution time [sec]") # + ggtitle("Comparison of two results of the benchmark")
        g <- g + xlab(lx) + ylab(ly)
       print(g)
 }
 
 args <- commandArgs(trailingOnly=TRUE)
-if (2 == length(args)){
+if (2 <= length(args)){
     dataX <- read.csv(args[1], header=T, sep=",", comment="#")
     dataY <- read.csv(args[2], header=T, sep=",", comment="#")
     l <- min(nrow(dataX), nrow(dataY))
@@ -26,14 +24,20 @@ if (2 == length(args)){
     dataY <- dataY[seq(1,l),]
     data <- cbind(dataX["num"], dataX[4], dataY[4])
     colnames(data) <- c("num", "X", "Y")
-    targetPDF=paste("scatter", ".pdf", sep="")
-    targetPNG=paste("scatter", ".png", sep="")
   } else {
-  print("USAGE: scatter.R data1 data2")
+  print("USAGE: scatter.R data1.csv data2.csv [graphName]")
+  }
+if (3 <= length(args)){
+    name <- args[3]
+    targetPDF <- paste("scatter-", name, ".pdf", sep="")
+    targetPNG <- paste("scatter-", name, ".png", sep="")
+  } else {
+     targetPDF <- "scatter.pdf"
+     targetPNG <- "scatter.png"
   }
 
 pdf(targetPDF, width=10, height=7)
-graph(data,args[1], args[2])
+graph(data, args[1], args[2])
 ggsave(targetPDF)
 ggsave(file=targetPNG, width=9, height=6, dpi=200)
 dev.off()
