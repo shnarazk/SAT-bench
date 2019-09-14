1bb0265435725346d66e39eb2156e15fddabb449
shujinarazaki <shujinarazaki@protonmail.com>
2019-09-14 18:07:40 +0900

    UNDER CONSTRUCTION for Serenity-0.7

--- src/bin/benchbot.rs
+++ src/bin/benchbot.rs
@@ -5,11 +5,13 @@ use sat_bench::utils::current_date_time;
 use sat_bench::{bench18::SCB, utils::parse_result};
 use serenity::builder::GetMessages;
 use serenity::client::{Client, Context, EventHandler};
-use serenity::command;
-use serenity::framework::standard::StandardFramework;
-use serenity::model::channel::Channel;
+use serenity::framework::standard::{StandardFramework,
+                                    CommandResult,
+                                    macros::{command, group}};
+use serenity::http::raw::Http;
+use serenity::model::channel::{Channel, Message};
 use serenity::model::event::TypingStartEvent;
-use serenity::model::gateway::Game;
+use serenity::model::gateway::Activity;
 use serenity::model::id::ChannelId;
 use serenity::model::prelude::Ready;
 use serenity::model::user::OnlineStatus;
@@ -85,6 +87,10 @@ pub struct Config {
     /// Discord token
     #[structopt(long = "token", default_value = "")]
     pub discord_token: String,
+    /// Your id for the bot to mention you
+    #[structopt(long = "your-id", default_value = "")]
+    pub master_id: String,
+
 }
 
 impl Default for Config {
@@ -105,6 +111,7 @@ impl Default for Config {
             run_name: String::new(),
             discord_channel: String::new(),
             discord_token: String::new(),
+            master_id: String::new(),
         }
     }
 }
@@ -141,13 +148,7 @@ fn main() {
     client.with_framework(
         StandardFramework::new()
             .configure(|c| c.prefix("."))
-            .cmd("clear", clean)
-            .cmd("draw", draw)
-            .cmd("start", start)
-            .cmd("whatsup", whatsup)
-            .cmd("who", who)
-            .cmd("help", help)
-            .cmd("bye", bye),
+            .group(&GENERAL_GROUP),
     );
     if let Err(why) = client.start() {
         println!("An error occurred while running the client: {:?}", why);
@@ -156,7 +157,7 @@ fn main() {
     }
 }
 
-fn start_benchmark() {
+fn start_benchmark(ctx: &mut Context) {
     let mut config = if let Ok(conf) = CONFIG.read() {
         conf.clone()
     } else {
@@ -251,25 +252,29 @@ fn start_benchmark() {
         let c = config.clone();
         thread::spawn(move || {
             thread::sleep(time::Duration::from_millis((2 + 2 * i as u64) * 1000));
-            worker(c);
+            worker(c, &ctx.http);
         });
     }
-    post(&format!(
-        "A new {} parallel benchmark starts.",
-        config.num_jobs,
-    ));
+    post(&ctx.http,
+         &format!(
+             "A new {} parallel benchmark starts.",
+             config.num_jobs,
+         )
+    );
     if !diff.is_empty() {
-        post(&format!(
-            "**WARNING: unregistered modifications**\n```diff\n{}```\n",
-            diff
-        ));
+        post(&ctx.http,
+            &format!(
+                 "**WARNING: unregistered modifications**\n```diff\n{}```\n",
+                 diff
+             )
+        );
     }
     if let Ok(mut conf) = CONFIG.write() {
         *conf = config.clone();
     }
 }
 
-fn worker(config: Config) {
+fn worker(config: Config, http: &Http) {
     loop {
         let mut p: PathBuf;
         let remains: usize;
@@ -288,22 +293,25 @@ fn worker(config: Config) {
         } else {
             break;
         }
-        execute(&config, n, &p);
+        execute(&config, http, n, &p);
         if remains == 0 {
             // I'm the last one.
-            state("");
+            state(http, "");
             let (s, u) = report(&config).unwrap_or((0, 0));
             if let Ok(mut answered) = ANSWERED.write() {
                 let sum = s + u;
                 *answered = sum;
-                post(&format!(
-                    "All ({} + {}, {}) problems were solved, answered {}.{}",
-                    config.target_from,
-                    config.target_step,
-                    config.target_to,
-                    sum,
-                    if 172 < sum { " It's a new record!" } else { "" },
-                ));
+                post(http,
+                     &format!(
+                         "<@{}>, All ({} + {}, {}) problems were solved, answered {}.{}",
+                         config.master_id,
+                         config.target_from,
+                         config.target_step,
+                         config.target_to,
+                         sum,
+                         if 172 < sum { " It's a new record!" } else { "" },
+                     )
+                );
             }
             let tarfile = config.sync_dir.join(&format!("{}.tar.xz", config.run_name));
             Command::new("tar")
@@ -320,7 +328,7 @@ fn worker(config: Config) {
                     .expect("fail to run sync command");
             }
             println!("Benchmark {} has been done.", config.run_name);
-        // process::exit(0);
+            // process::exit(0);
         } else {
             let pro = {
                 if let Ok(processed) = PROCESSED.read() {
@@ -365,23 +373,25 @@ fn worker(config: Config) {
                     || (pro == 380 && 134 < ans)
                     || (pro == 400 && 135 < ans)
                 {
-                    post(&format!(
-                        "New record: {} solutions at {}-th problem.",
-                        ans, pro
-                    ));
+                    post(http,
+                         &format!(
+                             "New record: {} solutions at {}-th problem.",
+                             ans, pro
+                         )
+                    );
                 }
             }
         }
     }
 }
 
-fn execute(config: &Config, num: usize, cnf: &PathBuf) {
+fn execute(config: &Config, http: &Http, num: usize, cnf: &PathBuf) {
     let f = PathBuf::from(cnf);
     if f.is_file() {
         let target: String = f.file_name().unwrap().to_str().unwrap().to_string();
         println!("\x1B[032mRunning on {}...\x1B[000m", target);
         if let Ok(answered) = ANSWERED.read() {
-            state(&format!("{}#{},{}", *answered, num, &target));
+            state(http, &format!("{}#{},{}", *answered, num, &target));
         }
         let mut command: Command = solver_command(config);
         for opt in config.solver_options.split_whitespace() {
@@ -389,13 +399,13 @@ fn execute(config: &Config, num: usize, cnf: &PathBuf) {
         }
         let result = command.arg(f.as_os_str()).output();
         match &result {
-            Err(_) => post(&format!("Something happened to {}.", &target)),
+            Err(_) => post(http, &format!("Something happened to {}.", &target)),
             Ok(r)
                 if String::from_utf8(r.stderr.clone())
-                    .unwrap()
-                    .contains("thread 'main' panicked") =>
+                .unwrap()
+                .contains("thread 'main' panicked") =>
             {
-                post(&format!("**Panic at {}.**", &target))
+                post(http, &format!("**Panic at {}.**", &target))
             }
             _ => (),
         }
@@ -428,28 +438,28 @@ fn solver_command(config: &Config) -> Command {
     }
 }
 
-fn post(mes: &str) {
+fn post(http: &Http, mes: &str) {
     if let Ok(cid) = CHID.read() {
-        if let Ok(channel) = ChannelId(*cid).to_channel() {
+        if let Ok(channel) = ChannelId(*cid).to_channel(http) {
             if let Channel::Guild(gchannel) = &channel {
                 let ch = gchannel.read();
                 if let Ok(id) = RUN.read() {
-                    ch.say(&format!("{}: {}", *id, mes)).expect("fail to say");
+                    ch.say(http, &format!("{}: {}", *id, mes)).expect("fail to say");
                 }
             }
         }
     }
 }
 
-fn state(s: &str) {
+fn state(http: &Http, s: &str) {
     if let Ok(cid) = CHID.read() {
-        if let Ok(channel) = ChannelId(*cid).to_channel() {
+        if let Ok(channel) = ChannelId(*cid).to_channel(http) {
             if let Channel::Guild(gchannel) = &channel {
                 if let Ok(mut mes) = M.write() {
                     *mes = s.to_string();
                 }
                 let ch = gchannel.read();
-                ch.broadcast_typing().expect("typing");
+                ch.broadcast_typing(http).expect("typing");
             }
         }
     }
@@ -566,38 +576,50 @@ impl EventHandler for Handler {
                 ctx.set_presence(None, OnlineStatus::Idle);
             } else {
                 let name = mes.to_string();
-                ctx.set_game(Game::playing(&name));
+                ctx.set_activity(Activity::playing(&name));
             }
         }
     }
 }
 
-command!(bye(_context, _message) {
-    process::exit(0);
+group!({
+    name: "general",
+    options: {},
+    commands: [bye, clean, draw, start, who, help, bye],
 });
 
-command!(clean(context, message) {
+
+#[command]
+fn bye(_context: &mut Context, _message: &Message) -> CommandResult {
+    process::exit(0);
+    // Ok(())
+}
+
+#[command]
+fn clean(context: &mut Context, message: &Message) -> CommandResult {
     context.idle();
     let ch = message.channel_id;
     let retriever = GetMessages::default().before(message.id).limit(100);
-    match ch.messages(|_| retriever) {
+    match ch.messages(context.http, |_| retriever) {
         Ok(ref v) => {
             let len = v.len();
             let mut n = 0;
             for (i, m) in v.iter().enumerate() {
-                match m.delete() {
+                match m.delete(context.http) {
                     Ok(_) => n += 1,
                     Err(why) => println!("{} ({}/{}): {}", n, i, len, why),
                 }
             }
         }
         Err(e) => {
-            ch.say(&format!("Error {}", e))?;
+            ch.say(context.http, &format!("Error {}", e))?;
         }
     }
-});
+    Ok(())
+}
 
-command!(draw(_context, message) {
+#[command]
+fn draw(context: &mut Context, message: &Message) -> CommandResult {
     println!("{:?}", message.content);
     if let Ok(conf) = CONFIG.read() {
         let host = {
@@ -615,37 +637,46 @@ command!(draw(_context, message) {
             .output()
             .is_err()
         {
-            message.channel_id.say("Failed to draw a cactus graph.").unwrap();
+            message.channel_id.say(context.http, "Failed to draw a cactus graph.").unwrap();
         }
         let cactus = conf.sync_dir.join(&format!("CactusL-{}.png", host));
         if cactus.exists() {
-            if message.channel_id.send_files(&[cactus], |m| m.content("Cactus Plot")).is_err() {
-                message.channel_id.say("Failed to upload the file.").unwrap();
+            if message.channel_id.send_files(context.http, &[cactus], |m| m.content("Cactus Plot")).is_err() {
+                message.channel_id.say(context.http, "Failed to upload the file.").unwrap();
             }
         } else {
-            message.channel_id.say(&format!("{} doesn't exist.", cactus.to_string_lossy())).unwrap();
+            message.channel_id.say(context.http, &format!("{} doesn't exist.", cactus.to_string_lossy())).unwrap();
         }
     }
-});
+    Ok(())
+}
 
-command!(start(_context, _message) {
-    start_benchmark();
-});
+#[command]
+fn start(context: &mut Context, _message: &Message) -> CommandResult {
+    start_benchmark(context);
+    Ok(())
+}
 
-command!(who(_context, message) {
+#[command]
+fn who(context: &mut Context, message: &Message) -> CommandResult {
     if let Ok(conf) = CONFIG.read() {
-        message.channel_id.say(&format!("I am {}: ```rust\n{:?}\n```", VERSION, conf))?;
+        message.channel_id.say(context.http, &format!("I am {}: ```rust\n{:?}\n```", VERSION, conf))?;
     }
-});
+    Ok(())
+}
 
-command!(help(_context, message) {
-    message.channel_id.say("I accept `bye`, `clear`, `draw`, `whatsup`, `who`".to_string())?;
-});
+#[command]
+fn help(context: &mut Context, message: &Message) -> CommandResult {
+    message.channel_id.say(context.http, "I accept `bye`, `clear`, `draw`, `whatsup`, `who`".to_string())?;
+    Ok(())
+}
 
-command!(whatsup(_context, message) {
+#[command]
+fn whatsup(context: &mut Context, message: &Message) -> CommandResult {
     if let Ok(p) = PROCESSED.read() {
         if let Ok(a) = ANSWERED.read() {
-            message.channel_id.say(&format!("tried {} problems, answered {}.", p, a))?;
+            message.channel_id.say(context.http, &format!("tried {} problems, answered {}.", p, a))?;
         }
     }
-});
+    Ok(())
+}
