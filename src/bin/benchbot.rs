d4b704a8b2e0a308c67c3f5c2b2621ccdfec3a3e
shujinarazaki <shujinarazaki@protonmail.com>
2019-10-10 09:33:00 +0900

    cargo update

--- src/bin/benchbot.rs
+++ src/bin/benchbot.rs
@@ -25,7 +25,7 @@ use std::sync::RwLock;
 use std::{env, process, time};
 use structopt::StructOpt;
 
-const VERSION: &str = "benchbot 0.5.12";
+const VERSION: &str = "benchbot 0.5.13";
 
 lazy_static! {
     pub static ref CHID: RwLock<u64> = RwLock::new(0);
