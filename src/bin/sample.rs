d2ea96732089974daba7e001281a979b20bd19bc
Narazaki, Shuji <shujinarazaki@protonmail.com>
2019-11-14 08:56:03 +0900

    under construction

--- src/bin/sample.rs
+++ src/bin/sample.rs
@@ -1,17 +1,27 @@
 use {
-    futures::{executor::block_on, join, pin_mut, stream::self, Stream},
-    std::{io, pin::Pin},
+    futures::{
+        executor::block_on,
+        join,
+        pin_mut,
+        stream,
+        stream::TryStreamExt,
+        Stream
+    },
+    std::{
+        io,
+        pin::Pin
+    },
 };
 
 fn main() {
     block_on(async { join!(run1(), run2()) });
-    let v: Vec<Result<usize, io::Error>> = vec![Ok(1), Ok(3), Ok(4)];
+    let v: Vec<Result<usize, io::Error>> = vec![Ok(1), Ok(2), Ok(4)];
     let s = stream::iter(v);
     pin_mut!(s);
     block_on(jump_around(s)).unwrap();
 }
 
- async fn run1() {
+async fn run1() {
     println!("Hello, world from 1.");
 }
 
@@ -22,13 +32,16 @@ async fn run2() {
 async fn jump_around(
     stream: Pin<&mut dyn Stream<Item = Result<usize, io::Error>>>,
 ) -> Result<(), io::Error> {
-    use futures::stream::TryStreamExt; // for `try_for_each_concurrent`
-    const MAX_CONCURRENT_JUMPERS: usize = 100;
-    stream.try_for_each_concurrent(MAX_CONCURRENT_JUMPERS, |num| async move {
-        // jump_n_times(num).await?;
-        report_n_jumps(num).await;
-        Ok(())
-    }).await?;
+    const MAX_CONCURRENT_JUMPERS: usize = 2;
+    stream
+        .try_for_each_concurrent(MAX_CONCURRENT_JUMPERS, |num| {
+            async move {
+                // jump_n_times(num).await?;
+                report_n_jumps(num).await;
+                Ok(())
+            }
+        })
+        .await?;
     Ok(())
 }
 
