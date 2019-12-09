80984a02d8e2f141e85ad8ddda75547af4aa4009
Narazaki, Shuji <shujinarazaki@protonmail.com>
2019-12-09 22:15:41 +0900

    ditto: fix spaces

--- src/bin/benchm.rs
+++ src/bin/benchm.rs
@@ -402,7 +402,7 @@ fn check_result(config: &Config) {
                     }
                     println!("{:>3},{:>3},{}", j, n.1, &r.0);
                 } else {
-                    print!("{}\x1B[032mRunning on {}th problem{}...\x1B[000m",
+                    print!("{}\x1B[032mRunning on the {}th problem {}...\x1B[000m",
                            CLEAR,
                            j,
                            SCB[j-1].1
