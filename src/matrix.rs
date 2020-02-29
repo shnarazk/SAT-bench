e997ff0336416a2bd8e593fefa88ef8aeb0976f2
Narazaki, Shuji <shujinarazaki@protonmail.com>
2020-02-29 17:31:42 +0900

    use blocking API of reqwest-0.10
    	modified:   Cargo.lock
    	modified:   Cargo.toml
    	modified:   src/matrix.rs

--- src/matrix.rs
+++ src/matrix.rs
@@ -5,11 +5,11 @@ use std::collections::HashMap;
 pub fn get_token<S: ::std::hash::BuildHasher>(map: &mut HashMap<&str, &str, S>) -> Option<String> {
     let url = "https://matrix.org/_matrix/client/r0/login";
     map.insert("type", "m.login.password");
-    let client = reqwest::Client::new();
+    let client = reqwest::blocking::Client::new();
     let res = client.post(url).json(&map).send();
     match res {
         Err(e) => println!("failed to post: {:?}.", e),
-        Ok(mut r) => {
+        Ok(r) => {
             let body: String = r.text().unwrap();
             let v: Value = serde_json::from_str(&body).unwrap();
             let token = v["access_token"].to_string();
@@ -30,7 +30,7 @@ pub fn post(room: &str, maybe_token: &Option<String>, msg: &str) {
         let mut map: HashMap<&str, &str> = HashMap::new();
         map.insert("msgtype", "m.text");
         map.insert("body", msg);
-        let client = reqwest::Client::new();
+        let client = reqwest::blocking::Client::new();
         let res = client.post(&url).json(&map).send();
         if res.is_err() {
             println!("failed to post.");
