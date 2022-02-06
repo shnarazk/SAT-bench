be2446985d3f2c93f0e3041aba4f9381ed8d7e44
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-02-06 21:17:51 +0900

    use async interface of reqwest

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,24 +1,20 @@
 /// Matrix.org IF
-use serde_json::Value;
-use std::collections::HashMap;
+use {futures::executor::block_on, serde_json::Value, std::collections::HashMap};
 
 pub fn get_token<S: ::std::hash::BuildHasher>(map: &mut HashMap<&str, &str, S>) -> Option<String> {
     let url = "https://matrix.org/_matrix/client/r0/login";
     map.insert("type", "m.login.password");
-    let client = reqwest::blocking::Client::new();
-    let res = client.post(url).json(&map).send();
-    match res {
-        Err(e) => println!("failed to post: {:?}.", e),
-        Ok(r) => {
-            let body: String = r.text().unwrap();
-            let v: Value = serde_json::from_str(&body).unwrap();
-            let token = v["access_token"].to_string();
-            assert!(!token.is_empty());
-            let s = &token[1..token.len() - 1];
-            return Some(s.to_string());
-        }
-    }
-    None
+    let client = reqwest::Client::new();
+    let s = async {
+        let content = client.post(url).json(&map).send().await.unwrap();
+        let text = content.text().await.unwrap();
+        let v: Value = serde_json::from_str(&text).unwrap();
+        let token = v["access_token"].to_string();
+        assert!(!token.is_empty());
+        let s = &token[1..token.len() - 1];
+        Some(s.to_string())
+    };
+    block_on(s)
 }
 
 pub fn post(room: &str, maybe_token: &Option<String>, msg: &str) {
@@ -30,10 +26,8 @@ pub fn post(room: &str, maybe_token: &Option<String>, msg: &str) {
         let mut map: HashMap<&str, &str> = HashMap::new();
         map.insert("msgtype", "m.text");
         map.insert("body", msg);
-        let client = reqwest::blocking::Client::new();
-        let res = client.post(&url).json(&map).send();
-        if res.is_err() {
-            println!("failed to post.");
-        }
+        let client = reqwest::Client::new();
+        let fut = async { client.post(&url).json(&map).send().await };
+        block_on(fut).expect("failed to post");
     }
 }
