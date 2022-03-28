988698cf8bdefc0ef543a7bcfeb2ee6939ae1c36
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-03-29 07:31:25 +0900

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,32 +1,37 @@
 {
   description = "A small utilities for SAT solver benchmarking";
-  inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs;
-    flake-utils.url = github:numtide/flake-utils;
-  };
+  inputs.nixpkgs.url = github:NixOS/nixpkgs;
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      defaultPackage =
-        with import nixpkgs { system = "${system}"; };
-        stdenv.mkDerivation rec {
-          name = "sat-bench-${version}";
-          pname = "sat-bench";
-          version = "0.14.1-20220327-1";
-          src = self;
-          buildInputs = rustc.buildInputs ++ [ cargo rustc libiconv openssl pkgconfig ];
-          buildPhase = "cargo build --release";
-          installPhase = ''
-              mkdir -p $out/bin;
-              install -t $out/bin target/release/sat-bench target/release/benchm
-              mkdir -p $out/lib
-              cp -r 3-SAT SAT09 SatRace2015 SC21 $out/lib/
-          '';
-          patchPhase = ''
-              sed -i "s|long = \"lib\", default_value = \"\"|long = \"lib\", default_value = \"$out/lib\"|" src/bin/sat-bench.rs
-          '';
-          SATBENCHLIB="$out/lib";
-        }
-      ;
-    })
-  ;
+  {
+    packages = builtins.listToAttrs
+      (map
+        (system:
+           with import nixpkgs { system = "${system}"; };
+           {
+             name = system;
+             value = {
+               default =
+                 stdenv.mkDerivation rec {
+                   name = "sat-bench-${version}";
+                   pname = "sat-bench";
+                   version = "0.14.1-20220327-1";
+                   src = self;
+                   buildInputs = rustc.buildInputs ++ [ cargo rustc libiconv openssl pkgconfig ];
+                   buildPhase = "cargo build --release";
+                   installPhase = ''
+                       mkdir -p $out/bin;
+                       install -t $out/bin target/release/sat-bench target/release/benchm
+                       mkdir -p $out/lib
+                       cp -r 3-SAT SAT09 SatRace2015 SC21 $out/lib/
+                   '';
+                   patchPhase = ''
+                       sed -i "s|long = \"lib\", default_value = \"\"|long = \"lib\", default_value = \"$out/lib\"|" src/bin/sat-bench.rs
+                   '';
+                   SATBENCHLIB="$out/lib";
+                 };
+           };
+        })
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ]
+      );
+  };
 }
