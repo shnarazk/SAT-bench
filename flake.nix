e69c2166974321661f5ebfd695ba44362ac3f52b
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-10-09 08:50:31 +0900

    Bump version up to 0.14.2

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
                  stdenv.mkDerivation rec {
                    name = "sat-bench-${version}";
                    pname = "sat-bench";
-                   version = "0.14.1-20220929-1";
+                   version = "0.14.2-20221009-1";
                    src = self;
                    buildInputs = rustc.buildInputs ++ [ cargo rustc libiconv openssl pkg-config ];
                    buildPhase = "cargo build --release";
