83c839d475055a4db84309c8b111fcbd966ae5b4
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-10-10 16:22:48 +0900

    new file:   src/bin/post.rs

---
+++ src/bin/post.rs
@@ -0,0 +1,11 @@
+use {matrix_sdk:: Client, std::env, url::Url};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let server_url = Url::parse("https://matrix.org").unwrap();
+    let client = Client::new(server_url).await.unwrap();
+    let mpasswd: String = env::var("MPASSWD").expect("Provide mpasswd");
+    let mid: String = env::var("MID").expect("Provide mid");
+    client.login_username(&mid, &mpasswd).send().await?;
+    Ok(())
+}
