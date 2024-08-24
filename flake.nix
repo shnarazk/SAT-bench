ea323f47578e8a6f2da7b43a0a3fdf1bfe9722b8
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-08-24 12:28:18 +0900

    update flake

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
                 default = rustPlatform.buildRustPackage rec {
                   name = "sat-bench-${version}";
                   pname = "sat-bench";
-                  version = "0.16.1-20240816-1";
+                  version = "0.16.1-20240824-1";
                   src = fetchFromGitHub {
                     name = "SAT-bench";
                     owner = "shnarazk";
