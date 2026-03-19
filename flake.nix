070ef71eb30f37a5db0b5e90dece9ffe24071421
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-03-20 08:08:04 +0900

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -13,13 +13,13 @@
                 default = rustPlatform.buildRustPackage rec {
                   name = "sat-bench-${version}";
                   pname = "sat-bench";
-                  version = "0.17.0-20260319-1";
+                  version = "0.17.0-20260320-1";
                   src = fetchFromGitHub {
                     name = "SAT-bench";
                     owner = "shnarazk";
                     repo = "SAT-bench";
-                    rev = "7faf828938e4cd819a9294148e37580f3440dde1";
-                    hash = "sha256-G/6wMo49JtwT2kI/ltdiza/J5kzNK3sQLgR0mEVexlI=";
+                    rev = "4ae892687c6401e716367b0acb9fd818c3f586c5";
+                    hash = "sha256-t2MfCv6oKrdGUbEA3lNcftMNnBO3ZNUnAg3l1EzbpwI";
                   };
                   cargoHash = "sha256-nB4evO7Ea6Oy7SUjyX3FUjasL/D+5lT87cxHdigrq4Q=";
                   buildInputs = rustc.buildInputs ++ [
